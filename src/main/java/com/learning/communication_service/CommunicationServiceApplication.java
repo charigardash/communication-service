16025de88a7c95dcf9cb270e4a783fd91210d054
charigardash <charigardash@gmail.com>
2025-11-21 05:33:56 +0530

    added custom rate limit logic

--- src/main/java/com/learning/communication_service/CommunicationServiceApplication.java
+++ src/main/java/com/learning/communication_service/CommunicationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.learning.communication_service;
 
+import com.common.base.BaseAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(BaseAutoConfiguration.class)
 public class CommunicationServiceApplication {
 
 	public static void main(String[] args) {
